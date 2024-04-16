62cc591f77bfb6aac7956ddd2a64fab888afe214
hamayamei <c0b231169d@edu.teu.ac.jp>
2024-04-16 16:46:04 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False) #練習7-1
     kk_img = pg.image.load("fig/3.png") #練習２　こうかとん画像
     kk_img = pg.transform.flip(kk_img, True, False) #練習２　画像、左右反転、上下反転
     tmr = 0
@@ -17,9 +18,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 800
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(kk_img, [300, 200])
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0]) #練習6
+        screen.blit(bg_img2, [-x+1600, 0]) #練習7-1
+        screen.blit(bg_img, [-x+3200, 0]) #練習7-2
+        screen.blit(bg_img2, [-x+4800, 0]) #練習7-2
+
+        screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
         clock.tick(200)
